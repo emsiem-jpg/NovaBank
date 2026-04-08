f46fb39465758ad52dbe304d96abad590129d122
emsiem-jpg <bluesiemieniuk@gmail.com>
2026-04-08 20:23:32 +0200

    added initial account model with deposit() and withdraw()

--- NovaBank.Domain/Account.cs
+++ NovaBank.Domain/Account.cs
@@ -1,10 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace NovaBank.Domain
+﻿namespace NovaBank.Domain
 {
-    internal class Account
+    public class Account
     {
+        const int AccountNumberLength = 10;
+        const int initialBalance = 0;
+        public Guid Id { get; set; }
+        public string AccountNumber { get; private set; }
+        public decimal Balance { get; private set; }
+
+        public Account(string accountNumber, decimal initialBalance)
+        {
+            Id = Guid.NewGuid();
+            AccountNumber = accountNumber;
+            Balance = initialBalance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Deposit amount must be greater than zero.");
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Withdrawal amount must be greater than zero.");
+            if (amount > Balance)
+                throw new InvalidOperationException("Insufficient funds.");
+            Balance -= amount;
+        }
+
     }
+
+
 }
